bd08b9dc8117a75253df42b31fa54686dc3a3a69
vinnugollakoti <vinnugollakoti289@gmail.com>
2024-04-22 17:24:54 +0530

    added about page

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
-
+const routes = require("./routes.js")
 const app = express()
 
 mongoose.connect("mongodb://localhost:27017/blogie")
 
+app.use("/", routes)
 app.use(express.json())
 
 app.listen(3001, () => {
