28736d190db57cce5603049e4bd4f586d63dea0b
vinnugollakoti <vinnugollakoti289@gmail.com>
2024-04-28 16:20:00 +0530

    added another login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Register from "./Components/Register.jsx"
 import Form from './Components/Form.jsx'
 import Blog from './Components/Blog.jsx'
 import Login from "./Components/Login.jsx"
+import Login1 from "./Components/Login1.jsx"
 import About from "./Components/About.jsx"
 import Forgotpass from "./Components/Forgotpass.jsx"
 import Resetpass from './Components/Resetpass.jsx'
@@ -18,6 +19,7 @@ const App = () => {
       <Route path="/register" element={<Register />} />
       <Route path="/form" element={<Form />} />
       <Route path="/login" element={<Login/>} />
+      <Route path="/login1" element={<Login1/>} />
       <Route path="/blog" element={<Blog />} />
       <Route path="/about" element={<About />} />
       <Route path="/resetpass" element={<Resetpass />} />
