03e25b73991231e7b745d172eb571f74fcd71a70
vinnugollakoti <vinnugollakoti289@gmail.com>
2024-05-02 10:38:19 +0530

    changed server.js file

--- server/server.js
+++ server/server.js
@@ -1,26 +1,44 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const routes = require("./routes.js");
-
+const Data = require("./schema.js");
+const dotenv = require("dotenv")
+const cookieParser = require("cookie-parser")
+const {UserRouter} = require("./routes/user.js")
 const app = express();
+dotenv.config()
+app.use(cors({
+  origin : "http://localhost:5173",
+  credentials : true
+}));
+app.use(cookieParser())
+app.use(express.json());
+app.use("/auth", UserRouter)
 
-// Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/blogie")
-    .then(() => {
-        console.log("MongoDB connected successfully");
-    })
-    .catch((error) => {
-        console.error("Error connecting to MongoDB:", error);
+app.post("/data", async (req, res) => {
+  const { author, email, heading, blog } = req.body;
+  const newBlog = new Data({
+    author,
+    email,
+    heading,
+    blog,
+  });
+  await newBlog.save();
+  return res.json({ status: true, message: "Blog is stored in DB" });
+});
+
+app.get("/getdata", async (req, res) => {
+  Data.find()
+    .then((data) => res.json(data))
+    .catch((err) => {
+      console.log("err is occured");
     });
+});
 
-// Middleware
-app.use(cors()); // Enable Cross-Origin Resource Sharing
-app.use(express.json()); // Parse JSON bodies
-app.use("/", routes); // Mount routes
 
-// Start the server
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT || 3001, async () => {
+  await mongoose.connect(
+    "mongodb://127.0.0.1:27017/blogie"
+  );
+  console.log("server is running");
 });
