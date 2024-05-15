faae7cb435a238815342de6e3e41050ea297def4
vinnugollakoti <vinnugollakoti289@gmail.com>
2024-05-15 14:40:50 +0530

    added ranks for authors

--- client/src/Components/Account.jsx
+++ client/src/Components/Account.jsx
@@ -1,5 +1,38 @@
 import profile from "../assets/profile.webp"
+import {useState, useEffect} from "react"
+import {useNavigate} from "react-router-dom"
+
+
 const Account = () => {
+  const navigate = useNavigate()
+  const [loginbtn, setLoginBtn] = useState(false);
+
+  function getCookie(name) {
+    let cookieArray = document.cookie.split('; ')
+    let cookie = cookieArray.find((row) => row.startsWith(name + '='))
+    return cookie ? cookie.split('=')[1] :""
+  }
+
+
+  useEffect(() => {
+    if (getCookie("authToken")) {
+      console.log("enjoy chey mowa")
+      setLoginBtn(true)
+    } else {
+      navigate("/login")
+    }
+  }, [navigate])
+
+
+  const handleLogout = () => {
+    document.cookie = "authToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    setLoginBtn(false);
+    navigate("/login");
+  };
+
+  const handleSettings = () => {
+    navigate("/settings")
+  }
   
   return (
     <div>
@@ -19,7 +52,7 @@ const Account = () => {
         </div>
         <hr />
         <div className="btns-div">
-          <button className="button2">
+          <button className="button2" onClick={handleSettings}>
               <svg
                 className="svg-icon"
                 fill="none"
@@ -40,7 +73,7 @@ const Account = () => {
               <span className="label2">Account</span>
           </button >
 
-          <button className="Btn3" >
+          <button className="Btn3" onClick={handleLogout} >
           <div className="sign3"><svg viewBox="0 0 512 512" className="svg3"><path className="path3" d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
   
           <div className="text3" >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Logout</div>
