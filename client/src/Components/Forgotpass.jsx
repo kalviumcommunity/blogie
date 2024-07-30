2a0eaf6ccc00f2b3daa3358a46c1f32df080d2a9
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2024-07-30 23:16:41 +0530

    final

--- client/src/Components/Forgotpass.jsx
+++ client/src/Components/Forgotpass.jsx
@@ -1,48 +1,43 @@
-import logo2 from "../assets/logo2.png";
-import { useState } from 'react';
-import axios from 'axios';
-import { useLocation, useNavigate } from "react-router-dom";
+import gmail from "../assets/gmail.svg";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-axios.defaults.withCredentials = true;
-
-const OTPValidation = () => {
-  const [otp, setOtp] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
+const Forgotpass = () => {
+  const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
-  const email=useLocation().state.email
-  console.log(email)
-  const valid=sessionStorage.getItem("qed-et")
+
+  const handleEmail = () => {
+    window.open("https://mail.google.com/mail/u/0/#spam", "_blank");
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
-    console.log(otp)
-    
+    setIsLoading(true);
     try {
-      const response = await axios.put("http://localhost:3001/auth/otpvalid", {
-        email,
-        valid,
-        otp,
-        password,
-      });
-      console.log(response);
-      navigate("/login");
+      const response = await axios.post("http://localhost:3001/auth/forgotpassword", { email });
+      if (response.data.status) {
+        alert("Check your email by clicking the email logo, please check spam emails for to reset password");
+        setTimeout(() => {
+          navigate("/login1");
+        }, 2000)
+      } else {
+        alert("your email is not registered")
+      }
     } catch (error) {
-      console.error("An error occurred during OTP validation:", error);
-      alert("OTP or Password incorrect!");
+      console.error("Error occurred while submitting form:", error);
     } finally {
       setTimeout(() => {
-        setLoading(false);
-      }, 1000);
+        setIsLoading(false);
+      }, 1000)
     }
   };
 
   return (
     <div>
-      <div className="main-page1">
-        <div>
-          {loading && (
+      <div>
+      {isLoading && (
             <div className="loader-container">
               <div className="spinner">
                 <div></div>
@@ -58,32 +53,61 @@ const OTPValidation = () => {
               </div>
             </div>
           )}
-          <h2 className="welcome">Welcome to</h2>
-          <img src={logo2} alt="" className="logo3" />
-        </div>
-        <div className="register-box">
-          <center><h2>Forgot Password?</h2></center>
-          <form action="" onSubmit={handleSubmit}>
+        <center>
+          <div className="oops">
+            <h2 className="welcome">Oops!</h2>
+            <h2 className="welcome1">Dont worry!</h2>
+            <h3 className="welcome2">We are here to get back your password</h3>
+          </div>
+        </center>
+        <div className="register-box new">
+          <center>
+            <h2>Forgot password</h2>
+          </center>
+          <form onSubmit={handleSubmit}>
             <div>
-              <label htmlFor="otp" className="label">OTP</label>
-              <input type="text" id="otp" onChange={(e) => setOtp(e.target.value)} required />
+              <label htmlFor="email">Enter your email : </label>
+              <input type="email" onChange={(e) => setEmail(e.target.value)} />
             </div>
-            <div>
-              <label htmlFor="password" className="label">New Password</label>
-              <input type="password" id="password" onChange={(e) => setPassword(e.target.value)} required />
-            </div>
-            <div>
-              {!loading ? (
-                <center>
-                  <button type="submit" className="login-btn" >Done!</button>
-                </center>
-              ) : null}
+            <div className="spsbtns">
+              <div className="sendbtn">
+                <button className="btn7" type="submit" disabled={isLoading}>
+                  {isLoading ? 'Sending...' : 'Send'}
+                  <div className="svg-wrapper-1">
+                    <div className="svg-wrapper">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        width="24"
+                        height="24"
+                      >
+                        <path fill="none" d="M0 0h24v24H0z"></path>
+                        <path
+                          fill="currentColor"
+                          d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+                        ></path>
+                      </svg>
+                    </div>
+                  </div>
+                </button>
+              </div>
+              <img
+                src={gmail}
+                alt=""
+                className="gmaillogo"
+                onClick={handleEmail}
+              />
             </div>
           </form>
+          <center>
+            <div className="suggestion">
+              <p>Open gmail and check for blogie's mail</p>
+            </div>
+          </center>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default OTPValidation;
+export default Forgotpass;
